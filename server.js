e49d5f8d63e748144620e77904bbe5edcd00fbcf
thenickben <40610922+thenickben@users.noreply.github.com>
2023-02-11 17:49:36 +0000

    change message

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const socketIO = require("socket.io");
+const express = require('express');
+const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = "/index.html";
+const INDEX = '/index.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
@@ -12,17 +12,17 @@ const server = express()
 
 const io = socketIO(server);
 
-io.on("connection", (socket) => {
-  console.log("Client connected");
-  socket.on("disconnect", () => console.log("Client disconnected"));
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 
   // receives a message from client
   // and replies back
-  socket.on("client-msg", (msg) => {
-    console.log("server log - server received a message: " + msg);
+  socket.on('client-msg', (msg) => {
+    console.log('server log - server received a message: ' + msg);
     socket.emit(
-      "server-reply",
-      "Thanks for the following message: '" + msg + "', very cute! ^^"
+      'server-reply',
+      "Thanks for the following message: '" + msg + "', very cuute! ^^"
     );
   });
 });
